e2d9bfbabc0d8794cd7ab7be2c29ff0721898a10
tuimac <tuimac.devadm01@gmail.com>
2020-02-09 17:32:37 +0900

    under development

--- wol/magicPacket.py
+++ wol/magicPacket.py
@@ -1,10 +1,12 @@
 import socket
+import re
 
 class MagicPacket:
     def __init__(self, mac):
         self.mac = mac
 
     def __createPacket(self):
+
 
     def sendPacket(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
