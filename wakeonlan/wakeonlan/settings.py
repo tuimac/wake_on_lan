4ad95f31f180ddc23243eed26aee6ec9c7662699
tuimac <tuimac.devadm01@gmail.com>
2020-05-15 15:09:55 +0000

    under development

--- wakeonlan/wakeonlan/settings.py
+++ wakeonlan/wakeonlan/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'index',
     'rest_framework'
 ]
 
@@ -57,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'wakeonlan'),
+            os.path.join(BASE_DIR, 'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
